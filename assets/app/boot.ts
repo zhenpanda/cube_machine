7ae77aa5a0aa80e4e6eac53acd56c9cfd6b6edb6
zhenpanda <zhentzhou@gmail.com>
2016-08-09 12:35:57 -0400

    make it better

--- assets/app/boot.ts
+++ assets/app/boot.ts
@@ -3,4 +3,8 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {AppComponent} from './app.components';
 
+import { provide, enableProdMode } from '@angular/core';
+
+enableProdMode();
+
 bootstrap(AppComponent);
